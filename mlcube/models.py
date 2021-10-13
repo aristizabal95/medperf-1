08950ce04e9585266b21885f1e379756fff4da12
Alejandro Aristizábal <alejandro.aristizabal@factored.ai>
2021-10-13 09:56:10 -0500

    Adjust some fields max length

--- mlcube/models.py
+++ mlcube/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class MlCube(models.Model):
 
     name = models.CharField(max_length=20)
-    git_mlcube_url = models.CharField(max_length=100)
-    git_parameters_url = models.CharField(max_length=100)
-    tarball_url = models.CharField(max_length=100, blank=True)
+    git_mlcube_url = models.CharField(max_length=256)
+    git_parameters_url = models.CharField(max_length=256)
+    tarball_url = models.CharField(max_length=256, blank=True)
     tarball_hash = models.CharField(max_length=100, blank=True)
-    owner =  models.ForeignKey(User, on_delete=models.PROTECT)
+    owner = models.ForeignKey(User, on_delete=models.PROTECT)
     metadata = models.JSONField(default=dict, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
@@ -18,4 +19,4 @@ class MlCube(models.Model):
 
     class Meta:
         verbose_name_plural = "MlCubes"
-        ordering = ['modified_at']
+        ordering = ["modified_at"]
